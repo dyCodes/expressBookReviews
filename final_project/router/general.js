4f445b685487267d8b5cb55919332950aa5ae8a2
dyCodes <yusufdaudu51@gmail.com>
2024-06-23 07:15:25 +0100

    feat: implement endpoint to get book review by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -60,10 +60,13 @@ public_users.get('/title/:title', function (req, res) {
 	return res.status(200).json(result);
 });
 
-//  Get book review
+// Get book review
 public_users.get('/review/:isbn', function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const isbn = req.params.isbn;
+	const book = books[isbn];
+
+	if (!book) return res.status(404).json({ error: 'Book not found!' });
+	return res.status(200).json({ reviews: book.review });
 });
 
 module.exports.general = public_users;
