d2b1633147a8f4ce02967ee18feb7d6f2a4aaa34
dyCodes <yusufdaudu51@gmail.com>
2024-06-23 22:41:47 +0100

    feat: Implement user login functionality

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,24 @@ app.use(express.json());
 
 app.use('/customer', session({ secret: 'fingerprint_customer', resave: true, saveUninitialized: true }));
 
+// Middleware
 app.use('/customer/auth/*', function auth(req, res, next) {
 	//Write the authenication mechanism here
+	if (req.session.auth) {
+		const token = req.session.auth.token;
+
+		// verify JWT token
+		jwt.verify(token, 'secret_key', (err, data) => {
+			if (err) {
+				return res.status(401).json({ error: 'Unauthorized access.' });
+			}
+			// set the user in the request object
+			req.user = data;
+			next();
+		});
+	} else {
+		return res.status(401).json({ error: 'User not authenticated.' });
+	}
 });
 
 const PORT = 5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -22,14 +22,25 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-	//returns boolean
-	//write code to check if username and password match the one we have in records.
+	// write code to check if username and password match the one we have in records.
+	let existingUser = users.find((user) => user.username === username && user.password === password);
+	return !!existingUser;
 };
 
 //only registered users can login
 regd_users.post('/login', (req, res) => {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const { username, password } = req.body;
+
+	if (!username || !password) return res.status(400).json({ message: 'Username and password are required.' });
+
+	if (authenticatedUser(username, password)) {
+		const token = jwt.sign({ username }, 'secret_key', { expiresIn: '4h' });
+		req.session.auth = { token };
+
+		return res.status(200).json({ message: 'Customer logged in successfully.' });
+	} else {
+		return res.status(401).json({ message: 'Invalid username or password.' });
+	}
 });
 
 // Add a book review
