bf5cc76a21fda7576260c682b1fdae15f2c46214
dyCodes <yusufdaudu51@gmail.com>
2024-06-24 05:37:00 +0100

    feat: update book endpoints to use async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,54 +21,78 @@ public_users.post('/register', async (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-	let result = books;
-	return res.status(200).json(result);
+public_users.get('/', async function (req, res) {
+	try {
+		const getBooks = () => new Promise((resolve) => resolve(books));
+		const result = await getBooks();
+		return res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json({ message: 'Error fetching books', error: error.message });
+	}
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-	const isbn = req.params.isbn;
-	const book = books[isbn];
-	if (!book) {
-		return res.status(404).json({ error: 'Book not found!' });
-	} else {
-		return res.status(200).json(book);
+public_users.get('/isbn/:isbn', async function (req, res) {
+	try {
+		const getBookDetails = (isbn) =>
+			new Promise((resolve, reject) => {
+				const book = books[isbn];
+				if (!book) {
+					reject('Book not found!');
+				} else resolve(book);
+			});
+
+		const isbn = req.params.isbn;
+		const result = await getBookDetails(isbn);
+		return res.status(200).json(result);
+	} catch (error) {
+		res.status(500).json({ error: error || 'Error fetching book details' });
 	}
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async (req, res) => {
 	const author = req.params.author;
-	const result = [];
 
-	// Obtain all the keys for the 'books' object
-	const bookKeys = Object.keys(books);
-	bookKeys.forEach((key) => {
-		if (books[key].author === author) {
-			result.push({ isbn: key, ...books[key] });
-		}
-	});
+	const getBooksByAuthor = (author) => {
+		return new Promise((resolve) => {
+			const filteredBooks = Object.keys(books)
+				.filter((key) => books[key].author === author)
+				.map((key) => ({ isbn: key, ...books[key] }));
+			resolve(filteredBooks);
+		});
+	};
 
-	if (result.length === 0) return res.status(404).json({ error: 'No books found for the author!' });
-	return res.status(200).json(result);
+	try {
+		const booksByAuthor = await getBooksByAuthor(author);
+		if (booksByAuthor.length === 0) return res.status(404).json({ error: 'No books found for the author!' });
+		return res.status(200).json(booksByAuthor);
+	} catch (error) {
+		return res.status(500).json({ error: 'Error fetching books by author' });
+	}
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
 	const title = req.params.title.toLowerCase();
-	const result = [];
 
-	// Obtain all the keys for the 'books' object
-	const bookKeys = Object.keys(books);
-	bookKeys.forEach((key) => {
-		if (books[key].title.toLowerCase() === title) {
-			result.push({ isbn: key, ...books[key] });
-		}
-	});
+	const getBooksByTitle = async (title) => {
+		return new Promise((resolve) => {
+			const filteredBooks = Object.keys(books)
+				.filter((key) => books[key].title.toLowerCase() === title)
+				.map((key) => ({ isbn: key, ...books[key] }));
+			resolve(filteredBooks);
+		});
+	};
 
-	if (result.length === 0) return res.status(404).json({ error: 'No books found with the given title!' });
-	return res.status(200).json(result);
+	try {
+		const booksByTitle = await getBooksByTitle(title);
+		if (booksByTitle.length === 0)
+			return res.status(404).json({ error: 'No books found with the given title!' });
+		return res.status(200).json(booksByTitle);
+	} catch (error) {
+		return res.status(500).json({ error: 'Error fetching books by title', details: error.message });
+	}
 });
 
 // Get book review
